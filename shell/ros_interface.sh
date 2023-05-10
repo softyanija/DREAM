49ecbad85218ee2b9351b22d6d3bf56697991c3f
softyanija <amakou0614@gmail.com>
2023-05-10 12:12:58 +0900

    add shell scripts to run ros_interface

---
+++ shell/ros_interface.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 scripts/launch_dream_ros.py -i train/best_network.pth -b base_footprint -v
---
+++ shell/ros_interface_timercam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 scripts/launch_dream_ros_timercam1.py -i train/best_network.pth -b base_footprint -v
